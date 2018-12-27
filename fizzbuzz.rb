7be8a2da1bacb24d38450ea2901fd0869869a1bb
ipsita Pattanaik <ipsitapattanaik.cse@gmail.com>
2018-12-27 02:42:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(x)
-  if fizz_3 = fizzbuzz(3)
+  if int % 3 == 0
 
     return "Fizz"
   elsif fizz_5 = fizzbuzz(5)
