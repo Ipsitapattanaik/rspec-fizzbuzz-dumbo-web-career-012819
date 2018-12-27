9bd07ebc940fa3c5a300a417e20d8e47d47b717c
ipsita Pattanaik <ipsitapattanaik.cse@gmail.com>
2018-12-27 03:04:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,18 +2,6 @@
 # See README.md for instructions on how to do this
 require 'rspec'
 def fizzbuzz(int)
-  if int % 3 == 0
 
-    return "Fizz"
-  elsif int % 5 == 0
-
-    return "Buzz"
-  elsif int % 15 == 0
-
-     return "FizzBuzz"
-  elsif int % 4 == 0
-
-    return "nil"
-  end
 end
 
