0bd87a6da89d0c465851cf8a1965de341bc815ac
ipsita Pattanaik <ipsitapattanaik.cse@gmail.com>
2018-12-27 02:48:46 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,23 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(int)
-  if int % 3 == 0
 
-    return "Fizz"
-  elsif int % 5 == 0
-
-    return "Buzz"
-  elsif int % 15 == 0
-
-     return "FizzBuzz"
-  elsif int % 4 == 0
-
-    return "nil"
-  end
-end
-fizzbuzz(3)
-fizzbuzz(5)
-fizzbuzz(4)
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
    return "FizzBuzz"
